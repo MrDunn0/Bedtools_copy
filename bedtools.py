e22931089d3c147807ad337de5cf6622c6850637
Tanya Maslikova <kosha44@yandex.ru>
2020-04-21 14:21:09 +0300

    add main file

---
+++ bedtools.py
@@ -0,0 +1,20 @@
+import parse_args
+import sort
+
+if __name__ == "__main__":
+    args = parse_args()
+    if args.command == 'sort':
+        pass
+
+    elif args.command == 'subtract':
+        pass
+
+    elif args.command == 'merge':
+        pass
+
+    elif args.command == 'intersect':
+        pass
+
+    elif args.command == 'getfasta':
+        pass
+
