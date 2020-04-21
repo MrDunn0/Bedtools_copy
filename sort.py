0e778231527d6b564bff0610e98b27e6036c0898
Tanya Maslikova <kosha44@yandex.ru>
2020-04-21 19:39:22 +0300

    add chrThenScore

--- sort.py
+++ sort.py
@@ -57,22 +57,6 @@ def size_ordering(line, sorted_list, order):
     return sorted_list
 
 
-# def add_to_desc_size_order(line, sorted_list):
-#     if not sorted_list:
-#         sorted_list.append(line)
-#         return sorted_list
-#     size = int(line[2]) - int(line[1])
-#     min_index, max_index = 0, len(sorted_list)
-#     while max_index - min_index > 0:
-#         med = (min_index + max_index) // 2
-#         if int(sorted_list[med][2]) - int(sorted_list[med][1]) < size:
-#             max_index = med
-#         else:
-#             min_index = med + 1
-#     sorted_list = sorted_list[:max_index] + [line] + sorted_list[max_index:]
-#     return sorted_list
-
-
 def apply_fun_to_file(function, file, storage, order="asc"):
     header, index_start_line = get_header_and_start_line(file)
     with open(file) as f:
@@ -115,27 +99,38 @@ def chr_then_size_ordering(line, sorted_dict, order="asc"):
         return sorted_dict
 
 
-# def add_line_to_chr_dict_sorted_by_size_desc(line, sorted_dict):
-#     chr, size = line[0], int(line[2]) - int(line[1])
-#     if chr not in sorted_dict:
-#         sorted_dict[chr] = [line]
-#         return sorted_dict
-#     else:
-#         min_pos, max_pos = 0, len(sorted_dict[chr])
-#         while max_pos - min_pos > 0:
-#             med = (min_pos + max_pos) // 2
-#             if int(sorted_dict[chr][med][2]) - int(sorted_dict[chr][med][1]) < size:
-#                 max_pos = med
-#             else:
-#                 min_pos = med + 1
-#         sorted_dict[chr] = \
-#             sorted_dict[chr][:max_pos] + [line] + sorted_dict[chr][max_pos:]
-#         return sorted_dict
+def chr_then_score_sorting(line, chr_dict_with_sorted_score, order="asc"):
+    chr, score = line[0], line[4]
+    if chr not in chr_dict_with_sorted_score:
+        chr_dict_with_sorted_score[chr] = [line]
+        return chr_dict_with_sorted_score
+    else:
+        min_pos, max_pos = 0, len(chr_dict_with_sorted_score[chr])
+        if order == "asc":
+            while max_pos - min_pos > 0:
+                med = (min_pos + max_pos) // 2
+                if chr_dict_with_sorted_score[chr][med][4] > score:
+                    max_pos = med
+                else:
+                    min_pos = med + 1
+            chr_dict_with_sorted_score[chr] = \
+                chr_dict_with_sorted_score[chr][:max_pos] + [line] + chr_dict_with_sorted_score[chr][max_pos:]
+        elif order == "desc":
+            while max_pos - min_pos > 0:
+                med = (min_pos + max_pos) // 2
+                if chr_dict_with_sorted_score[chr][med][4] < score:
+                    max_pos = med
+                else:
+                    min_pos = med + 1
+            chr_dict_with_sorted_score[chr] = \
+                chr_dict_with_sorted_score[chr][:max_pos] + [line] + chr_dict_with_sorted_score[chr][max_pos:]
+        return chr_dict_with_sorted_score
 
 
 def sort_by_default(file):
     '''by chromosome then start position in ascending'''
     return apply_fun_to_file(chr_then_start_ordering, file, dict())
+
 
 def sizeA(file):
     '''Sort by feature size in ascending order'''
@@ -157,6 +152,16 @@ def chrThenSizeD(file):
     return apply_fun_to_file(chr_then_size_ordering, file, dict(), "desc")
 
 
+def chrThenScoreA(file):
+    '''Sort by chromosome (asc), then by score (asc).'''
+    return apply_fun_to_file(chr_then_score_sorting, file, dict(), "asc")
+
+
+def chrThenScoreD(file):
+    '''Sort by chromosome (asc), then by score (asc).'''
+    return apply_fun_to_file(chr_then_score_sorting, file, dict(), "desc")
+
+
 def write_for_chr_sorted(tuple_header_and_chr_dict_with_sorted_start):
     header, chr_dict_with_sorted_start = tuple_header_and_chr_dict_with_sorted_start
     with open("sorted_file.txt", "w") as f:
@@ -175,13 +180,10 @@ def write_for_size(tuple_header_sorted_list):
             f.write("\t".join(line))
             f.write("\n")
 
-# def chrThenScoreA(file):
-#     '''Sort by chromosome (asc), then by score (asc).'''
-#     return apply_fun_to_file()
 
-write_for_chr_sorted(sort_by_default("unsorted.bed"))
-write_for_size(sizeA("unsorted.bed"))
-write_for_size(sizeD("unsorted.bed"))
-write_for_chr_sorted(chrThenSizeA("unsorted.bed"))
-write_for_chr_sorted(chrThenSizeD("unsorted.bed"))
-
+# write_for_chr_sorted(sort_by_default("unsorted.bed"))
+# write_for_size(sizeA("unsorted.bed"))
+# write_for_size(sizeD("unsorted.bed"))
+# write_for_chr_sorted(chrThenSizeA("unsorted.bed"))
+# write_for_chr_sorted(chrThenSizeD("unsorted.bed"))
+# write_for_chr_sorted(chrThenScoreA("unsorted.bed"))
