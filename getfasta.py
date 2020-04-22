b35807436881ec05b302c60c9a95a442478d2ff4
Yulia-Yakovleva <st041958@student.spbu.ru>
2020-04-22 03:40:49 +0300

    НЕУЖЕЛИ ТЕПЕРЬ МОЖНО НОРМАЛЬНО ПАРСИТЬ ФАСТУ

--- getfasta.py
+++ getfasta.py
@@ -1,25 +1,47 @@
-import parse_args
+class SeqObject():
 
-args = parse_args()
-if args.command == 'getfasta':
-    if args.fi:
-        print(args.fi)
+    def __init__(self, seq_id=None, description=None, name=None, sequence=None):
+        self.seq_id = seq_id
+        self.description = description
+        self.name = name
+        self.sequence = sequence
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def __repr__(self):
+        return str(self.__dict__)
 
 
-# open and parse fasta file
-def parse_fasta(file):
-    sequences = {}
-    with open(file, 'r') as in_f:
-        sequence_name = None
-        sequence = []
-        for line in in_f:
-            line = line.strip()
-            if line.startswith(">"):
-                if sequence:
-                    sequences[sequence_name] = "".join(sequence)
-                    sequence = []
-                sequence_name = line[1:]
-            else:
-                sequence.append(line)
-        sequences[sequence_name] = "".join(sequence)
-    return sequences
+def iterate_on_fasta(file):
+    header, sequence = None, []
+    for line in file:
+        line = line.rstrip()
+        if line.startswith(">"):
+            if header:
+                yield header, ''.join(sequence)
+            header, sequence = line, []
+        else:
+            sequence.append(line)
+    if header:
+        yield header, ''.join(sequence)
+
+
+def fasta_tuple_to_object(tuple):
+    name = tuple[0].lstrip('>')
+    seq_id = name.split(' ')[0]
+    description = ' '.join(name.split(' ')[1:])
+    sequence = tuple[1]
+    seq_obj = SeqObject(name=name, seq_id=seq_id,
+                        description=description,
+                        sequence=sequence)
+    return seq_obj
+
+
+def parse_sequences(filename):
+    with open(filename, 'r') as text_file:
+        list_of_objects = []
+        fasta_tuples = iterate_on_fasta(text_file)
+        for element in fasta_tuples:
+            list_of_objects.append(fasta_tuple_to_object(element))
+        return list_of_objects
